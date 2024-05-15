0e49d01e0b7ecb6bdd8e8e45d36e110935ef165f
Wild-Hound <wildhound404@gmail.com>
2024-05-15 14:27:58 +0600

    hitbox added

--- js/player.js
+++ js/player.js
@@ -22,32 +22,54 @@ class Player extends Sprite {
   }
 
   update() {
+    const ctx = this.ctx;
     this.position.x += this.velocity.x;
+    this.updateHitBox();
 
     this.checkHorizontalCollision();
 
     this.applyGravity();
 
+    this.updateHitBox();
+
     this.checkVerticalCollision();
+  }
+
+  updateHitBox() {
+    this.hitBox = {
+      position: {
+        x: this.position.x + 60,
+        y: this.position.y + 33,
+      },
+      width: 50,
+      height: 54,
+    };
   }
 
   checkHorizontalCollision() {
     for (let i = 0; i < collisionBlocks.length; i++) {
       const collisionBlock = collisionBlocks[i];
       if (
-        this.position.x <= collisionBlock.position.x + collisionBlock.width &&
-        this.position.x + this.width >= collisionBlock.position.x &&
-        this.position.y + this.height >= collisionBlock.position.y &&
-        this.position.y <= collisionBlock.position.y + collisionBlock.height
+        this.hitBox.position.x <=
+          collisionBlock.position.x + collisionBlock.width &&
+        this.hitBox.position.x + this.hitBox.width >=
+          collisionBlock.position.x &&
+        this.hitBox.position.y + this.hitBox.height >=
+          collisionBlock.position.y &&
+        this.hitBox.position.y <=
+          collisionBlock.position.y + collisionBlock.height
       ) {
         if (this.velocity.x < 0) {
+          const offset = this.hitBox.position.x - this.position.x;
           this.position.x =
-            collisionBlock.position.x + collisionBlock.width + 0.01;
+            collisionBlock.position.x + collisionBlock.width - offset + 0.01;
           break;
         }
 
         if (this.velocity.x > 0) {
-          this.position.x = collisionBlock.position.x - this.width - 0.01;
+          const offset =
+            this.hitBox.position.x - this.position.x + this.hitBox.width;
+          this.position.x = collisionBlock.position.x - offset - 0.01;
           break;
         }
       }
@@ -63,21 +85,28 @@ class Player extends Sprite {
     for (let i = 0; i < collisionBlocks.length; i++) {
       const collisionBlock = collisionBlocks[i];
       if (
-        this.position.x <= collisionBlock.position.x + collisionBlock.width &&
-        this.position.x + this.width >= collisionBlock.position.x &&
-        this.position.y + this.height >= collisionBlock.position.y &&
-        this.position.y <= collisionBlock.position.y + collisionBlock.height
+        this.hitBox.position.x <=
+          collisionBlock.position.x + collisionBlock.width &&
+        this.hitBox.position.x + this.hitBox.width >=
+          collisionBlock.position.x &&
+        this.hitBox.position.y + this.hitBox.height >=
+          collisionBlock.position.y &&
+        this.hitBox.position.y <=
+          collisionBlock.position.y + collisionBlock.height
       ) {
         if (this.velocity.y < 0) {
           this.velocity.y = 0;
+          const offset = this.hitBox.position.y - this.position.y;
           this.position.y =
-            collisionBlock.position.y + collisionBlock.height + 0.01;
+            collisionBlock.position.y + collisionBlock.height - offset + 0.01;
           break;
         }
 
         if (this.velocity.y > 0) {
           this.velocity.y = 0;
-          this.position.y = collisionBlock.position.y - this.height - 0.01;
+          const offset =
+            this.hitBox.position.y - this.position.y + this.hitBox.height;
+          this.position.y = collisionBlock.position.y - offset - 0.01;
           break;
         }
       }
